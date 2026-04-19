21400d4cab0a3ea481421bb5006f405456d1b3c8
kevin <fabianuskevin147@gmail.com>
2026-04-19 19:08:01 +0700

    another preview image web metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export const metadata: Metadata = {
     siteName: "PMKRI Cabang Purwokerto",
     images: [
       {
-        url: "https://pmkri-purwokerto.vercel.app/cover-web-pmkri-pwt.png",
+        url: `{https://pmkri-purwokerto.vercel.app/cover-web-pmkri-pwt.png?t=${Date.now()}`,
         secureUrl: "https://pmkri-purwokerto.vercel.app/cover-web-pmkri-pwt.png",
         width: 1200,
         height: 630,
